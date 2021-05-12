17723b1c071219ab7808de7792dba9a32fbc1c29
Zoe Liu <zliu27@u.rochester.edu>
2021-05-11 21:40:53 -0400

    set up

---
+++ bookBank.js
@@ -0,0 +1,7 @@
+const data = [
+    { id: 1, title: "Homo Deus", genre: "Nonfiction", name: "Yuval Noah Harari", zoeRating: 5 },
+    { id: 2, title: "Sapiens: A Brief History of Humankind", genre: "Nonfiction", name: "Yuval Noah Harari", zoeRating: 3 },
+    { id: 3, title: "Where the Crawdads Sing", genre: "Novel", name: "Delia Owens", zoeRating: 5 },
+    { id: 4, title: "Let My People Go Surfing", genre: "Biography", name: "Yvon Chouinard", zoeRating: 4 },
+    { id: 5, title: "I'm Thinking of Ending Things", genre: "Thriller", name: "Iain Reid", zoeRating: 1 },
+  ];
